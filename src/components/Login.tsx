139893dc23b3f40d03c31a4b8d05f6eeb24824a1
lamula21 <jvaldiv8@montgomerycollege.edu>
2023-12-27 02:32:00 -0500

    fix: url redirect from signOut

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -235,7 +235,7 @@ export default function Login() {
 
 								signOut({
 									redirect: false,
-									callbackUrl: "http://localhost:3000/",
+									callbackUrl: "/",
 								})
 
 								toast.message(
